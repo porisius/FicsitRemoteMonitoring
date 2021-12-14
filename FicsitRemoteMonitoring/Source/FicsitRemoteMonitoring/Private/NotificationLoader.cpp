6bd9582ada1896671ddf9018dfb1ecd98bb578e2
porisius <47161774+porisius@users.noreply.github.com>
2021-12-14 01:03:11 -0500

    Dev Build for 0.6.9.420-Feyko

---
+++ FicsitRemoteMonitoring/Source/FicsitRemoteMonitoring/Private/NotificationLoader.cpp
@@ -0,0 +1,18 @@
+// Copyright Coffee Stain Studios. All Rights Reserved.
+
+
+#include "NotificationLoader.h"
+
+#include "FicsitRemoteMonitoringModule.h"
+#include "Misc/FileHelper.h"
+#include "Misc/Paths.h"
+
+bool UNotificationLoader::FileLoadString(FString LoadFile, FString& LoadString)
+{
+	return FFileHelper::LoadFileToString(LoadString, *(LoadFile));
+}
+
+bool UNotificationLoader::FileSaveString(FString SaveString, FString SaveFile)
+{
+	return FFileHelper::SaveStringToFile(SaveString, *(SaveFile));
+}
