52412135f83bc5b6947b353a17ba371e1ccd60a3
GalaxyVOID <94946192+GalaxyVOID@users.noreply.github.com>
2023-03-11 18:06:02 +0200

    Update 3/11/2023
    Fixed sort buttons flashing.
    Reworked the table code.

--- www/js/getDrone.js
+++ www/js/getDrone.js
@@ -13,25 +13,84 @@ async function catchDrone() {
   displayDrone(getDrone);
 }
 
+var dTable = document.createElement("table");
+dTable.style.width = "100%";
+dTable.className = "table";
+document.getElementById("DroneTable").appendChild(dTable);
+
 function displayDrone(getDrone) {
-  var i, txt;
+  var tr = document.createElement("tr");
+  tr.className = "topTable";
+  dTable.appendChild(tr);
 
-  txt = "<table class='table' width=100%>";
-  txt += "<tr class='topTable'>";
-  txt +=
-    "<td><b>Home Station:</b></td><td><b>Paired Station:</b></td><td><b>Drone Status:</b></td><td><b>Incoming Transfer Rate:</b></td><td><b>Outgoing Transfer Rate:</b></td><td><b>Battery Usage Rate:</b></td></tr>";
+  var lines = [
+    "Home Station:",
+    "Paired Station:",
+    "Drone Status:",
+    "Incoming Transfer Rate:",
+    "Outgoing Transfer Rate:",
+    "Battery Usage Rate:",
+  ];
 
-  for (i in getDrone) {
-    txt += "<tr>";
-    txt += "<td>" + getDrone[i].HomeStation + "</td>";
-    txt += "<td>" + getDrone[i].PairedStation + "</td>";
-    txt += "<td>" + getDrone[i].DroneStatus + "</td>";
-    txt += "<td>" + getDrone[i].AvgTotalIncRate + "</td>";
-    txt += "<td>" + getDrone[i].AvgTotalOutRate + "</td>";
-    txt += "<td>" + getDrone[i].EstBatteryRate + "</td>";
-    txt += "</tr>";
+  for (var i = 0; i < lines.length; i++) {
+    var td = document.createElement("td");
+    var b = document.createElement("b");
+    b.innerText = lines[i];
+    td.appendChild(b);
+
+    var sortContainer = document.createElement("div");
+    sortContainer.classList = "sortContainer";
+    td.appendChild(sortContainer);
+
+    var asc = document.createElement("button");
+    asc.classList = "sortAsc";
+    asc.innerText = "⬆️";
+    asc.onclick = sort;
+
+    var desc = document.createElement("button");
+    desc.classList = "sortDesc";
+    desc.innerText = "⬇️";
+    desc.onclick = sort;
+
+    sortContainer.appendChild(asc);
+    sortContainer.appendChild(desc);
+
+    tr.appendChild(td);
   }
 
-  txt += "</table>";
-  document.getElementById("DroneTable").innerHTML = txt;
+  for (var i in getDrone) {
+    var tr = document.createElement("tr");
+    dTable.appendChild(tr);
+
+    var homeStation = document.createElement("td");
+    homeStation.innerText = getDrone[i].HomeStation;
+
+    var pairedStation = document.createElement("td");
+    pairedStation.innerText = getDrone[i].PairedStation;
+
+    var droneStatus = document.createElement("td");
+    droneStatus.innerText = getDrone[i].DroneStatus;
+
+    var avgTotalIncRate = document.createElement("td");
+    avgTotalIncRate.innerText = getDrone[i].AvgTotalIncRate;
+
+    var avgTotalOutRate = document.createElement("td");
+    avgTotalOutRate.innerText = getDrone[i].AvgTotalOutRate;
+
+    var estBatteryRate = document.createElement("td");
+    estBatteryRate.innerText = getDrone[i].EstBatteryRate;
+
+    var trLoop = [
+      homeStation,
+      pairedStation,
+      droneStatus,
+      avgTotalIncRate,
+      avgTotalOutRate,
+      estBatteryRate,
+    ];
+
+    for (var i in trLoop) {
+      tr.appendChild(trLoop[i]);
+    }
+  }
 }
--- www/js/map.js
+++ www/js/map.js
@@ -1,131 +1,146 @@
 const map = new L.map("map", {
-    crs: L.CRS.Simple,
-    maxZoom: 18,
-    minZoom: -10,
-    fullscreenControl: true,
+  crs: L.CRS.Simple,
+  maxZoom: 18,
+  minZoom: -10,
+  fullscreenControl: true,
 });
 
 function setupMousePos() {
-    L.Control.MousePosition = L.Control.extend({
-        options: {
-            position: "topleft",
-            emptyString: "Unavailable",
-            numDigits: 5,
-        },
+  L.Control.MousePosition = L.Control.extend({
+    options: {
+      position: "topleft",
+      emptyString: "Unavailable",
+      numDigits: 5,
+    },
 
-        onAdd: function (map) {
-            this._container = L.DomUtil.create(
-                "div",
-                "leaflet-control-mouseposition"
-            );
-            L.DomEvent.disableClickPropagation(this._container);
-            map.on("mousemove", this._onMouseMove, this);
-            this._container.innerHTML = this.options.emptyString;
-            return this._container;
-        },
+    onAdd: function (map) {
+      this._container = L.DomUtil.create(
+        "div",
+        "leaflet-control-mouseposition"
+      );
+      L.DomEvent.disableClickPropagation(this._container);
+      map.on("mousemove", this._onMouseMove, this);
+      this._container.innerHTML = this.options.emptyString;
+      return this._container;
+    },
 
-        onRemove: function (map) {
-            map.off("mousemove", this._onMouseMove);
-        },
+    onRemove: function (map) {
+      map.off("mousemove", this._onMouseMove);
+    },
 
-        _onMouseMove: function (e) {
-            const x = L.Util.formatNum(e.latlng.lng, this.options.numDigits);
-            const y = L.Util.formatNum(-e.latlng.lat, this.options.numDigits);
-            this._container.innerHTML = `x:${x} y:${y}`;
-        },
-    });
+    _onMouseMove: function (e) {
+      const x = L.Util.formatNum(e.latlng.lng, this.options.numDigits);
+      const y = L.Util.formatNum(-e.latlng.lat, this.options.numDigits);
+      this._container.innerHTML = `x:${x} y:${y}`;
+    },
+  });
 
-    L.Map.mergeOptions({
-        positionControl: false,
-    });
+  L.Map.mergeOptions({
+    positionControl: false,
+  });
 
-    L.Map.addInitHook(function () {
-        if (this.options.positionControl) {
-            this.positionControl = new L.Control.MousePosition();
-            this.addControl(this.positionControl);
-        }
-    });
+  L.Map.addInitHook(function () {
+    if (this.options.positionControl) {
+      this.positionControl = new L.Control.MousePosition();
+      this.addControl(this.positionControl);
+    }
+  });
 
-    L.control.mousePosition = function (options) {
-        return new L.Control.MousePosition(options);
-    };
+  L.control.mousePosition = function (options) {
+    return new L.Control.MousePosition(options);
+  };
 }
 
 function setupMap() {
+  const bounds = [
+    [375e3, 425301.832031],
+    [-375e3, -324698.832031],
+  ];
 
-    const bounds = [
-        [375e3, 425301.832031],
-        [-375e3, -324698.832031]
-    ];
+  L.imageOverlay("/img/map.png", bounds).addTo(map);
+  map.fitBounds(bounds);
+  L.control.mousePosition().addTo(map);
 
-    L.imageOverlay('/img/map.png', bounds).addTo(map);
-    map.fitBounds(bounds);
-    L.control.mousePosition().addTo(map);
+  map.attributionControl.setPrefix("");
 
-    map.attributionControl.setPrefix('');
+  map.setView([0, 0], -13);
 
-    map.setView([0,0] ,-13);
-
-    updateMap();
+  updateMap();
 }
 
 function updateMap() {
+  const bounds = [
+    [375e3, 425301.832031],
+    [-375e3, -324698.832031],
+  ];
 
+  map.eachLayer((layer) => {
+    layer.remove();
+  });
 
-    const bounds = [
-        [375e3, 425301.832031],
-        [-375e3, -324698.832031]
-    ];
+  L.imageOverlay("/img/map.png", bounds).addTo(map);
 
-    map.eachLayer((layer) => {
-        layer.remove();
-      });
-
-    L.imageOverlay('/img/map.png', bounds).addTo(map);
-
-    createRealtimeLayer("/getPlayer", "Player");
-    createRealtimeLayer("/getDrone", "Drone");
-    createRealtimeLayer("/getVehicles", "Vehicles");
-    createRealtimeLayer("/getTrains", "Trains");
-    
+  createRealtimeLayer("/getPlayer", "Player");
+  createRealtimeLayer("/getDrone", "Drone");
+  createRealtimeLayer("/getVehicles", "Vehicles");
+  createRealtimeLayer("/getTrains", "Trains");
 }
 
 async function createRealtimeLayer(url, type, container = null) {
-    const response = await fetch(url);
-    const data = await response.text();
-    const getGeo = JSON.parse(data);
+  const response = await fetch(url);
+  const data = await response.text();
+  const getGeo = JSON.parse(data);
 
-    for (var i=0; i<getGeo.length; i++) {
+  for (var i = 0; i < getGeo.length; i++) {
+    //This doesn't make sense, but necessary
+    var lat = getGeo[i].location.y * -1;
+    var lon = getGeo[i].location.x;
+    var markerLocation = new L.LatLng(lat, lon);
+    var marker = new L.Marker(markerLocation);
 
-        //This doesn't make sense, but necessary
-        var lat = getGeo[i].location.y * -1;
-        var lon = getGeo[i].location.x;
-        var markerLocation = new L.LatLng(lat, lon);
-        var marker = new L.Marker(markerLocation);
-
-        if (type == "Player") {
-            var popupText = "Player: " + getGeo[i].PlayerName + "<br>Ping Time: " + getGeo[i].PingTime + " ms";
-            marker.setIcon(greenIcon);
-        } else if (type == "Drone") {
-            var popupText = "Destination: " + getGeo[i].CurrentDestination + "<br>Flying Mode: " + getGeo[i].CurrentFlyingMode;
-            marker.setIcon(yellowIcon);
-        } else if (type == "Trains") {
-            var popupText = "Train Name: " + getGeo[i].TrainName + "<br>Speed: " + getGeo[i].ForwardSpeed + "<br>Derailed: " + getGeo[i].Derailed + "<br>Train Station: " + getGeo[i].TrainStation;
-            marker.setIcon(redIcon);
-        } else if (type == "Vehicles") {
-            var popupText = "Vehicle Type: " + getGeo[i].VehicleType + "<br>AutoPilot: " + getGeo[i].AutoPilot;
-            marker.setIcon(greyIcon);
-        }
-
-        map.addLayer(marker);
-        marker.bindPopup(popupText);
-
+    if (type == "Player") {
+      var popupText =
+        "Player: " +
+        getGeo[i].PlayerName +
+        "<br>Ping Time: " +
+        getGeo[i].PingTime +
+        " ms";
+      marker.setIcon(greenIcon);
+    } else if (type == "Drone") {
+      var popupText =
+        "Destination: " +
+        getGeo[i].CurrentDestination +
+        "<br>Flying Mode: " +
+        getGeo[i].CurrentFlyingMode;
+      marker.setIcon(yellowIcon);
+    } else if (type == "Trains") {
+      var popupText =
+        "Train Name: " +
+        getGeo[i].TrainName +
+        "<br>Speed: " +
+        getGeo[i].ForwardSpeed +
+        "<br>Derailed: " +
+        getGeo[i].Derailed +
+        "<br>Train Station: " +
+        getGeo[i].TrainStation;
+      marker.setIcon(redIcon);
+    } else if (type == "Vehicles") {
+      var popupText =
+        "Vehicle Type: " +
+        getGeo[i].VehicleType +
+        "<br>AutoPilot: " +
+        getGeo[i].AutoPilot;
+      marker.setIcon(greyIcon);
     }
+
+    map.addLayer(marker);
+    marker.bindPopup(popupText);
+  }
 }
 
 function main() {
-    setupMousePos();
-    setupMap();
+  setupMousePos();
+  setupMap();
 }
 
 main();
