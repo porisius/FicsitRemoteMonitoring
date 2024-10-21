cd74dc25953b17d02dc1092c66ac48280bd69877
derpierre65 <git@derpierre65.de>
2024-10-21 16:33:03 +0100

    feat: improve usages text in /frm command

--- Source/FicsitRemoteMonitoring/Private/Commands/multi.cpp
+++ Source/FicsitRemoteMonitoring/Private/Commands/multi.cpp
@@ -1,12 +1,24 @@
-
 #include "Commands/multi.h"
 #include <regex>
 
 FChatReturn AFRMCommand::RemoteMonitoringCommand(UObject* WorldContext, class UCommandSender* Sender, TArray<FString> Arguments) {
-
 	FChatReturn ChatReturn;
-	FLinearColor color = FLinearColor::Red;
-	FString chat;
+
+	ChatReturn.Color = FLinearColor::Red;
+	ChatReturn.Status = EExecutionStatus::UNCOMPLETED;
+
+	int32 argumentsNum = Arguments.Num();
+
+	if (argumentsNum == 0) {
+		ChatReturn.Chat = TEXT(
+			"Usage:\n"
+			"/frm debug <file/info> <Endpoint>\n"
+			"/frm http <start/stop>\n"
+			"/frm serial <start/stop>"
+		);
+
+		return ChatReturn;
+	}
 
 	FString command = Arguments[0].ToLower();
 
@@ -14,6 +26,11 @@ FChatReturn AFRMCommand::RemoteMonitoringCommand(UObject* WorldContext, class UC
 	fgcheck(ModSubsystem);
 
 	if (command == "debug") {
+		ChatReturn.Chat = TEXT("Usage: /frm debug <file/info> <Endpoint>");
+
+		if (argumentsNum < 3) {
+			return ChatReturn;
+		}
 
 		FString OutputType = Arguments[1].ToLower();
 		FString sEndpoint = Arguments[2];
@@ -22,17 +39,12 @@ FChatReturn AFRMCommand::RemoteMonitoringCommand(UObject* WorldContext, class UC
 		FString Json = ModSubsystem->HandleEndpoint(WorldContext, sEndpoint, bSuccess);
 
 		if (!bSuccess) {
-
 			ChatReturn.Chat = TEXT("Unable to find endpoint, please refer to the documentation at docs.ficsit.app or the mod creator.");
-			ChatReturn.Color = FLinearColor::Red;
-			ChatReturn.Status = EExecutionStatus::UNCOMPLETED;
 
 			return ChatReturn;
-
 		}
 
 		if (OutputType == "file") {
-
 			FString JsonPath = FPaths::ProjectDir() + "Mods/FicsitRemoteMonitoring/Debug/" + sEndpoint + ".json";
 
 			UNotificationLoader::FileSaveString(Json, JsonPath);
@@ -40,36 +52,26 @@ FChatReturn AFRMCommand::RemoteMonitoringCommand(UObject* WorldContext, class UC
 			ChatReturn.Chat = TEXT("Data saved to Mod location in the Debug folder.");
 			ChatReturn.Color = FLinearColor::White;
 			ChatReturn.Status = EExecutionStatus::COMPLETED;
-
-			return ChatReturn;
-
 		}
 		else if (OutputType == "info") {
-
 			ChatReturn.Chat = Json;
 			ChatReturn.Color = FLinearColor::White;
 			ChatReturn.Status = EExecutionStatus::COMPLETED;
-
-			return ChatReturn;
-
 		}
 
-		else {
-
-			ChatReturn.Chat = TEXT("Usage: /frm debug <file/info> <Endpoint>");
-			ChatReturn.Color = FLinearColor::Red;
-			ChatReturn.Status = EExecutionStatus::UNCOMPLETED;
-
-			return ChatReturn;
-
-		}
+		return ChatReturn;
 	}
-	else if (command == "http") {
+	
+	if (command == "http") {
+		ChatReturn.Chat = TEXT("Usage: /frm http <start/stop>");
+
+		if (argumentsNum < 2) {
+			return ChatReturn;
+		}
 
 		FString arg1 = Arguments[1].ToLower();
 
 		if (arg1 == "start") {
-
 			auto config = FConfig_HTTPStruct::GetActiveConfig(WorldContext);
 			int32 Port = config.HTTP_Port;
 			
@@ -78,11 +80,8 @@ FChatReturn AFRMCommand::RemoteMonitoringCommand(UObject* WorldContext, class UC
 			ChatReturn.Chat = FString(TEXT("HTTP Service Initiated on Port: " + FString::FromInt(Port)));
 			ChatReturn.Color = FLinearColor::Green;
 			ChatReturn.Status = EExecutionStatus::COMPLETED;
+
 			UE_LOG(LogHttpServer, Log, TEXT("HTTP Service started. Port: %d"), Port);
-
-
-			return ChatReturn;
-
 		}
 		else if (arg1 == "stop") {
 			ModSubsystem->StopWebSocketServer();
@@ -91,24 +90,21 @@ FChatReturn AFRMCommand::RemoteMonitoringCommand(UObject* WorldContext, class UC
 			ChatReturn.Chat = TEXT("Stopping HTTP Service.");
 			ChatReturn.Color = FLinearColor::White;
 			ChatReturn.Status = EExecutionStatus::COMPLETED;
-
-			return ChatReturn;
 		}
 
-		ChatReturn.Chat = TEXT("Usage: /frm http <start/stop>");
-		ChatReturn.Color = FLinearColor::Red;
-		ChatReturn.Status = EExecutionStatus::UNCOMPLETED;
-
 		return ChatReturn;
-
 	}
 
 	if (command == "serial") {
+		ChatReturn.Chat = TEXT("Usage: /frm serial <start/stop>");
+
+		if (argumentsNum < 2) {
+			return ChatReturn;
+		}
 
 		FString arg1 = Arguments[1].ToLower();
 
 		if (arg1 == "start") {
-
 			auto config = FConfig_SerialStruct::GetActiveConfig(WorldContext);
 			FString Port = config.COM_Port;
 
@@ -117,11 +113,8 @@ FChatReturn AFRMCommand::RemoteMonitoringCommand(UObject* WorldContext, class UC
 			ChatReturn.Chat = FString(TEXT("Serial/RS232 Service Initiated on Port: " + Port));
 			ChatReturn.Color = FLinearColor::Green;
 			ChatReturn.Status = EExecutionStatus::COMPLETED;
+
 			UE_LOGFMT(LogHttpServer, Log, "Serial/RS232 Service started. Port: {Port}");
-
-
-			return ChatReturn;
-
 		}
 		else if (arg1 == "stop") {
 			ModSubsystem->StopWebSocketServer();
@@ -130,44 +123,27 @@ FChatReturn AFRMCommand::RemoteMonitoringCommand(UObject* WorldContext, class UC
 			ChatReturn.Chat = TEXT("Stopping Serial/RS232 Service.");
 			ChatReturn.Color = FLinearColor::White;
 			ChatReturn.Status = EExecutionStatus::COMPLETED;
-
-			return ChatReturn;
 		}
 
-		ChatReturn.Chat = TEXT("Usage: /frm serial <start/stop>");
-		ChatReturn.Color = FLinearColor::Red;
-		ChatReturn.Status = EExecutionStatus::UNCOMPLETED;
-
 		return ChatReturn;
-
 	}
 
 	if (command == "icon") {
-
 		if (!UKismetSystemLibrary::IsDedicatedServer(WorldContext)) {
-
 			ModSubsystem->IconGenerator_BIE();
 
 			ChatReturn.Chat = TEXT("Icon Generatation Completed");
 			ChatReturn.Color = FLinearColor::Green;
 			ChatReturn.Status = EExecutionStatus::COMPLETED;
-
 		}
 		else {
-
 			ChatReturn.Chat = TEXT("Dedicated Servers do not have graphical assets to extract. Cancelling... Generate locally, then upload to server.");
-			ChatReturn.Color = FLinearColor::Red;
-			ChatReturn.Status = EExecutionStatus::UNCOMPLETED;
-
 		}
 
 		return ChatReturn;
-
 	}
 
-	ChatReturn.Chat = TEXT("Unable to find command type, please refer to the documentation at docs.ficsit.app.");
-	ChatReturn.Color =  FLinearColor::Red;
-	ChatReturn.Status = EExecutionStatus::UNCOMPLETED;
+	ChatReturn.Chat = TEXT("Unable to find command " + command + ", please refer to the documentation at docs.ficsit.app.");
 
 	return ChatReturn;
 };
