904261b4686d674c17f1d10ee7d11fbf9e8c0122
Chris Wall <47161774+porisius@users.noreply.github.com>
2025-04-14 12:10:48 -0400

    feature: CSS API Controller

---
+++ Source/FicsitRemoteMonitoring/Private/FRM_Controller.cpp
@@ -0,0 +1,18 @@
+﻿#include "FRM_Controller.h"
+
+#include "FGDSSharedTypes.h"
+#include "../Public/FicsitRemoteMonitoring.h"
+#include "../Public/FRM_RequestData.h"
+#include "Logging/StructuredLog.h"
+#include "FicsitRemoteMonitoringModule.h"
+
+FFGServerErrorResponse UFRM_Controller::Handler_getPower(FString &OutEndpoint)
+{
+	FRequestData RequestData;
+	bool bSuccess = false;
+	int32 ErrorCode = 404;
+	
+	ModSubsystem->HandleEndpoint("getPower", RequestData, bSuccess, ErrorCode, OutEndpoint);
+	
+	return FFGServerErrorResponse::Ok();
+}
