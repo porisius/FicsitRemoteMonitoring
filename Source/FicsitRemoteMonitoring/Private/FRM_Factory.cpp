4669f28771ff889d776e9568324406cc1c63b182
derpierre65 <git@derpierre65.de>
2025-04-17 15:32:34 +0100

    feat: add more fields

--- Source/FicsitRemoteMonitoring/Private/FRM_Factory.cpp
+++ Source/FicsitRemoteMonitoring/Private/FRM_Factory.cpp
@@ -82,19 +82,83 @@ TArray<TSharedPtr<FJsonValue>> UFRM_Factory::getElevators(UObject* WorldContext,
 		for (int32 i = 0; i < Elevator->GetNumFloorStops(); i++)
 		{
 			TSharedPtr<FJsonObject> JStop = MakeShared<FJsonObject>();
-			FElevatorFloorStopInfo Test = Elevator->GetFloorStopInfoByIndex(i);
-			JStop->Values.Add("Name", MakeShared<FJsonValueString>(Test.FloorName));
-			JStop->Values.Add("Height", MakeShared<FJsonValueNumber>(Test.Height));
+			FElevatorFloorStopInfo FloorStop = Elevator->GetFloorStopInfoByIndex(i);
+			JStop->Values.Add("Name", MakeShared<FJsonValueString>(FloorStop.FloorName));
+			JStop->Values.Add("Height", MakeShared<FJsonValueNumber>(FloorStop.Height));
 			JStops.Add(MakeShared<FJsonValueObject>(JStop));
 		}
-		
-		JElevator->Values.Add("Name", MakeShared<FJsonValueString>(Elevator->mDisplayName.ToString()));
+
+		TArray<TSharedPtr<FJsonValue>> JOccupyingCharacters;
+		TArray<AFGCharacterPlayer*> OccupyingCharacters = Elevator->GetOccupyingCharacters();
+		for (AFGCharacterPlayer* OccupyingCharacter : OccupyingCharacters )
+		{
+			TSharedPtr<FJsonObject> JOccupyingCharacter = UFRM_Library::CreateBaseJsonObject(OccupyingCharacter);
+			JOccupyingCharacter->Values.Add("Name", MakeShared<FJsonValueString>(UFRM_Library::GetPlayerName(OccupyingCharacter)));
+			JOccupyingCharacters.Add(MakeShared<FJsonValueObject>(JOccupyingCharacter));
+		}
+
+		FString Status;
+		FString ReadableStatus;
+		switch (Elevator->mElevatorState)
+		{
+		case EElevatorState::EES_IdleAtFloor:
+			Status = ReadableStatus = TEXT("Idle");
+			break;
+		case EElevatorState::EES_ArrivedAtFloor:
+			Status = ReadableStatus = TEXT("Arrived");
+			break;
+		case EElevatorState::EES_PauseBeforeDoorOpen:
+			Status = TEXT("PauseBeforeDoorOpen");
+			ReadableStatus = TEXT("Paused Before Opening Doors");
+			break;
+		case EElevatorState::EES_WaitingAtFloor:
+			Status = ReadableStatus = TEXT("Waiting");
+			break;
+		case EElevatorState::EES_DoorsOpening:
+			Status = TEXT("DoorsOpening");
+			ReadableStatus = TEXT("Opening Doors");
+			break;
+		case EElevatorState::EES_DoorsClosing:
+			Status = TEXT("DoorsClosing");
+			ReadableStatus = TEXT("Closing Doors");
+			break;
+		case EElevatorState::EES_PauseBeforeMove:
+			Status = TEXT("PauseBeforeMove");
+			ReadableStatus = TEXT("Pause Before Moving");
+			break;
+		case EElevatorState::EES_Moving:
+			Status = ReadableStatus = TEXT("Moving");
+			break;
+		case EElevatorState::EES_PausePowerOut:
+			Status = TEXT("NoPower");
+			ReadableStatus = TEXT("No Power");
+			break;
+		case EElevatorState::EES_SafetyStopping:
+			Status = TEXT("SafetyStopping");
+			ReadableStatus = TEXT("Stopped Due to No Power");
+			break;
+		default:
+			Status = TEXT("Unknown");
+		}
+
 		JElevator->Values.Add("ClassName", MakeShared<FJsonValueString>(UKismetSystemLibrary::GetClassDisplayName(Elevator->GetClass())));
-		JElevator->Values.Add("location", MakeShared<FJsonValueObject>(UFRM_Library::getActorJSON(Cast<AActor>(Elevator))));
+		JElevator->Values.Add("Status", MakeShared<FJsonValueString>(Status));
+		JElevator->Values.Add("ReadableStatus", MakeShared<FJsonValueString>(ReadableStatus));
+		JElevator->Values.Add("Speed", MakeShared<FJsonValueNumber>(Elevator->GetElevatorElevatorSpeed()));
+		JElevator->Values.Add("DurationOfCurrentState", MakeShared<FJsonValueNumber>(Elevator->GetDurationOfCurrentElevatorState()));
+		JElevator->Values.Add("Height", MakeShared<FJsonValueNumber>(Elevator->GetElevatorHeight()));
+		JElevator->Values.Add("CurrentFloorStep", MakeShared<FJsonValueNumber>(Elevator->GetIndexOfFloorStop(Elevator->GetCurrentFloorStop())));
 		JElevator->Values.Add("NumFloorStops", MakeShared<FJsonValueNumber>(Elevator->GetNumFloorStops()));
-		JElevator->Values.Add("ActiveFloorStep", MakeShared<FJsonValueNumber>(Elevator->GetIndexOfFloorStop(Elevator->GetCurrentFloorStop())));
+		JElevator->Values.Add("NumOccupyingCharacters", MakeShared<FJsonValueNumber>(OccupyingCharacters.Num()));
+		JElevator->Values.Add("HasPower", MakeShared<FJsonValueBoolean>(Elevator->HasPower()));
+		JElevator->Values.Add("IsOccupied", MakeShared<FJsonValueBoolean>(Elevator->IsElevatorOccupied()));
+		JElevator->Values.Add("location", MakeShared<FJsonValueObject>(UFRM_Library::getActorJSON(Cast<AActor>(Elevator))));
+		JElevator->Values.Add("OccupyingCharacters", MakeShared<FJsonValueArray>(JOccupyingCharacters));
 		JElevator->Values.Add("FloorStops", MakeShared<FJsonValueArray>(JStops));
-		JElevator->Values.Add("features", MakeShared<FJsonValueObject>(UFRM_Library::getActorFeaturesJSON(Cast<AActor>(Elevator), Elevator->mDisplayName.ToString(), Elevator->mDisplayName.ToString())));
+		JElevator->Values.Add("features", MakeShared<FJsonValueObject>(UFRM_Library::getActorFeaturesJSON(
+			Cast<AActor>(Elevator), Elevator->mDisplayName.ToString(),
+		  Elevator->mDisplayName.ToString()
+		)));
 
 		JConveyorBeltArray.Add(MakeShared<FJsonValueObject>(JElevator));
 	}
